f21217dd72b4133005867d461e72ca797bb8e55e
Ben (work) <bscorwin@gmail.com>
2015-12-23 12:21:33 -0600

    Added howtoplay page

--- TheDrawingGame/urls.py
+++ TheDrawingGame/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 from game import views
 
 urlpatterns = patterns('',
@@ -7,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^wait/(?P<code>.*)', views.wait_round, name='wait_game'),
     url(r'^reset/(?P<code>.*)', views.reset_round, name='reset_game'),
     url(r'^game/', include('game.urls')),
+    url(r'^howtoplay/', TemplateView.as_view(template_name='howtoplay.html')),
     url(r'^view/(?P<code>.*)', views.view_game, name='view_game'),
     url(r'^$', views.home, name="home"),
 )
